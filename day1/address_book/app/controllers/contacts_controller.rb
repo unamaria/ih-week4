7b22281a53db2622025da8b53024171ca17f80ec
Maria <unamaria@gmail.com>
2015-07-06 20:14:40 +0200

    Index names with link to show contact

--- day1/address_book/app/controllers/contacts_controller.rb
+++ day1/address_book/app/controllers/contacts_controller.rb
@@ -1,10 +1,12 @@
 class ContactsController < ApplicationController
   def index
+    @contacts = Contact.order("name ASC")
   end
   def create
   end
   def destroy
   end
   def show
+    @contact = Contact.find params[:id]
   end
 end
