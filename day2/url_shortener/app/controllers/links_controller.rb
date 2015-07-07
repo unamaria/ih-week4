de2446590fecc99b457510bf745f139958990e67
Maria <unamaria@gmail.com>
2015-07-07 19:15:40 +0200

    Prepend http if not present

--- day2/url_shortener/app/controllers/links_controller.rb
+++ day2/url_shortener/app/controllers/links_controller.rb
@@ -4,11 +4,22 @@ class LinksController < ApplicationController
   end
 
   def create
-    @link = Link.create(original_url: params[:original_url])
+    original_url = check_http(params[:original_url])
+    @link = Link.create(original_url: original_url)
     redirect_to links_path
   end
 
   def short_to_original
     redirect_to Link.find_by(short_url: params[:shortlink]).original_url
+  end
+
+  private
+
+  def check_http(original_url)
+    if !original_url.start_with?("http://")
+      original_url = original_url.prepend("http://")
+    end
+
+    original_url
   end
 end
