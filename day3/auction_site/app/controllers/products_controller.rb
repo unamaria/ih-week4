7848c6a31504595431e1f4303b77995d7d031c36
Maria <unamaria@gmail.com>
2015-07-09 16:05:15 +0200

    Implement creation of new products

--- day3/auction_site/app/controllers/products_controller.rb
+++ day3/auction_site/app/controllers/products_controller.rb
@@ -8,14 +8,27 @@ class ProductsController < ApplicationController
 	end
 
 	def new
-
+		@product = Product.new
 	end
 
 	def create
+		@product = Product.new product_params
 
+		if @product.valid? && @product.present?
+			@product.save
+			redirect_to product_path(@product), notice: "Product sucessfully added"
+		else
+			render :new
+		end
 	end
 
 	def destroy
 
+	end
+
+	private
+
+	def product_params
+		params.require(:product).permit(:name, :description, :deadline, :user_id)
 	end
 end
