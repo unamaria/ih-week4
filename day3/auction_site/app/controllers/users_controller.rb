9f8987c699600a9411b2bba73985faf9e1461906
Maria <unamaria@gmail.com>
2015-07-09 15:44:34 +0200

    Display errors and notices

--- day3/auction_site/app/controllers/users_controller.rb
+++ day3/auction_site/app/controllers/users_controller.rb
@@ -8,10 +8,23 @@ class UsersController < ApplicationController
 	end
 
 	def create
+		@user = User.new user_params
 
+		if @user.valid? && @user.present?
+			@user.save
+			redirect_to user_path(@user), notice: "User #{@user.name} added correctly"
+		else
+			render :new
+		end
 	end
 
 	def destroy
 
+	end
+
+	private
+
+	def user_params
+		params.require(:user).permit(:name, :email)
 	end
 end
