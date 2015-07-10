928c00e7257126d769b6aae80e697ba922aa5239
Maria <unamaria@gmail.com>
2015-07-10 13:22:54 +0200

    Create form to add concerts

--- day4/concert_network/app/controllers/concerts_controller.rb
+++ day4/concert_network/app/controllers/concerts_controller.rb
@@ -4,4 +4,12 @@ class ConcertsController < ApplicationController
 		end_of_today = DateTime.current.at_end_of_day
 		@concerts = Concert.where("date >= ? AND date < ?", beginning_of_today, end_of_today)
 	end
+
+	def new
+		@concert = Concert.new
+	end
+
+	def create
+
+	end
 end
