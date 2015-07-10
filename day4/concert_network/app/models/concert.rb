8ba363890e4af2e1d102a913e84bc5b063bd9904
Maria <unamaria@gmail.com>
2015-07-10 18:02:33 +0200

    Before adding bootstrap

--- day4/concert_network/app/models/concert.rb
+++ day4/concert_network/app/models/concert.rb
@@ -2,7 +2,6 @@ class Concert < ActiveRecord::Base
 	has_many :comments, dependent: :destroy
 
 	def self.sort_by_comments(number=10)
-		concerts = self.all.sort { |x,y| y.comments.count <=> x.comments.count }
-		concerts = concerts[0..9]
+		self.all.sort { |x,y| y.comments.count <=> x.comments.count }[0..9]
 	end
 end
