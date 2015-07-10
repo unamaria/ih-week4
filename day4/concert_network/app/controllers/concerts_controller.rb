ecf4e0fcbba34c2557c1e0a1539723fd868b303c
Maria <unamaria@gmail.com>
2015-07-10 13:30:37 +0200

    Create concerts (no time, no notice, no redirect to show

--- day4/concert_network/app/controllers/concerts_controller.rb
+++ day4/concert_network/app/controllers/concerts_controller.rb
@@ -10,6 +10,17 @@ class ConcertsController < ApplicationController
 	end
 
 	def create
+		concert = Concert.new concert_params
+		if concert.save
+			redirect_to concerts_path
+		else
+			render :new
+		end
+	end
 
+	private
+
+	def concert_params
+		params.require(:concert).permit(:band, :venue, :city, :date, :price, :description)
 	end
 end
